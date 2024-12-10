c90ff01c613a26f101bbc3fac77c4db1bd64cafa
cw2 <cw2@users.noreply.github.com>
2024-12-10 18:58:58 +0100

    AoC 2024 Day 5

---
+++ AdventOfCode2024.Day05.Part1App/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode2024.Day05.Part1App;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        using var reader = File.OpenText("input.txt");
+
+        var rules = new HashSet<string>();
+
+        while (reader.ReadLine() is { Length: > 0 } rule)
+        {
+            rules.Add(rule); // x|y where x is preceding and y is succeeding page number
+        }
+        // Empty line between rules and update page
+
+        var sumOfMiddlePageNumbers = 0;
+
+        while (reader.ReadLine() is { } update)
+        {
+            // Update is a comma-separated list of page numbers
+            var numbers = update.Split(',').Select(int.Parse).ToArray();
+
+            var rightOrder = true;
+
+            for (var i = 0; i < numbers.Length - 1; i++)
+            {
+                for (var j = i + 1; j < numbers.Length; j++)
+                {
+                    if (!rules.Contains(numbers[i] + "|" + numbers[j]))
+                    {
+                        rightOrder = false;
+                        break;
+                    }
+                }
+            }
+
+            if (rightOrder)
+            {
+                System.Diagnostics.Debug.Assert(int.IsOddInteger(numbers.Length));
+                sumOfMiddlePageNumbers += numbers[numbers.Length / 2];
+            }
+        }
+
+        Console.WriteLine(sumOfMiddlePageNumbers); // 6505
+    }
+}
---
+++ AdventOfCode2024.Day05.Part2App/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace AdventOfCode2024.Day05.Part2App;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        using var reader = File.OpenText("input.txt");
+
+        var rules = new HashSet<string>();
+
+        while (reader.ReadLine() is { Length: > 0 } rule)
+        {
+            rules.Add(rule); // x|y where x is preceding and y is succeeding page number
+        }
+        // Empty line between rules and update page
+
+        var sumOfMiddlePageNumbers = 0;
+
+        while (reader.ReadLine() is { } update)
+        {
+            // Update is a comma-separated list of page numbers
+            var numbers = update.Split(',').ToArray();
+
+            // For each of the incorrectly-ordered updates, use the page ordering rules
+            // to put the page numbers in the right order
+            if (IsWrongOrder_Swap(numbers, rules: rules))
+            {
+                while (IsWrongOrder_Swap(numbers, rules: rules))
+                {
+                    // Do nothing
+                }
+
+                System.Diagnostics.Debug.Assert(int.IsOddInteger(numbers.Length));
+                sumOfMiddlePageNumbers += int.Parse(numbers[numbers.Length / 2]);
+            }
+        }
+
+        Console.WriteLine(sumOfMiddlePageNumbers); // 6897
+    }
+
+    // Whilst it is not really ideal to have the swapping included here,
+    // it is much simpler than fiddling with return tuple or out parameters
+    private static bool IsWrongOrder_Swap(string[] numbers, HashSet<string> rules)
+    {
+        for (var i = 0; i < numbers.Length - 1; i++)
+        {
+            for (var j = i + 1; j < numbers.Length; j++)
+            {
+                if (!rules.Contains(numbers[i] + "|" + numbers[j]))
+                {
+                    (numbers[i], numbers[j]) = (numbers[j], numbers[i]); // Swap
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
