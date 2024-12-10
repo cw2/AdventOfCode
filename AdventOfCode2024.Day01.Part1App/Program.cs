38353e8ff9d2d6bcefad0379df89fc69761496e3
cw2 <cw2@users.noreply.github.com>
2024-12-10 18:57:40 +0100

    AoC 2024 Day 1

---
+++ AdventOfCode2024.Day01.Part1App/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.StringSplitOptions;
+
+namespace AdventOfCode2024.Day01.Part1App;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var inputLines = File.ReadAllLines("input.txt");
+
+        var numbers1 = new List<int>();
+        var numbers2 = new List<int>();
+
+        foreach(var parts in inputLines.Select(line => line.Split(' ', TrimEntries | RemoveEmptyEntries)))
+        {
+            numbers1.Add(int.Parse(parts[0]));
+            numbers2.Add(int.Parse(parts[1]));
+        }
+
+        var sumOfDistances = numbers1.Order().Zip(numbers2.Order(), (n1, n2) => Math.Abs(n1 - n2)).Sum();
+
+        Console.WriteLine(sumOfDistances);
+    }
+}
---
+++ AdventOfCode2024.Day01.Part2App/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.StringSplitOptions;
+
+namespace AdventOfCode2024.Day01.Part2App;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var inputLines = File.ReadAllLines("input.txt");
+
+        var left = new List<int>();
+        var right = new Dictionary<int, int>();
+
+        foreach (var parts in inputLines.Select(line => line.Split(' ', TrimEntries | RemoveEmptyEntries)))
+        {
+            left.Add(int.Parse(parts[0]));
+
+            var r = int.Parse(parts[1]);
+            right[r] = right.GetValueOrDefault(r, 0) + 1;
+        }
+
+        var totalSimilarityScore = left.Sum(x => x * right.GetValueOrDefault(x, 0));
+
+        Console.WriteLine(totalSimilarityScore);
+    }
+}
